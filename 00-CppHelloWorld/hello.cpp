a023c7c83bdb76a8d9e8ec1ed72a8a79cbfd860d
impanetta-utnfrba <imartinezpanetta@frba.utn.edu.ar>
2026-04-15 17:36:06 -0300

    Update hello.cpp

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -4,3 +4,5 @@ int main()
 {
     std::cout << "Hello World!\n";
 }
+
+// Luego de compilar y ejecutar el código para verificar su correcto funcionamiento, envié por terminal el siguiente comando: ".\hello.exe > salida.txt" que redirrecciona la salida por terminal a un archivo .txt.
